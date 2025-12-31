f1ec576d0c8cd8770d53a5c78fd39459415b26cf
ravindujayathilaka <00prabashwara123@gmail.com>
2025-12-31 23:57:20 +1300

    feat: implement register and login endpoints

--- api/src/server.ts
+++ api/src/server.ts
@@ -2,12 +2,20 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
+import authRoutes from "./routes/auth.routes";
+import { authMiddleware } from "./middlewares/auth.middleware";
+
 dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/auth", authRoutes);
+app.get("/me", authMiddleware, (req, res) => {
+  res.json({ userId: (req as any).userId });
+});
 
 app.get("/health", (_, res) => {
   res.json({ status: "ok" });
